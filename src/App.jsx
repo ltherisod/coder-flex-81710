99e8de803d2407b0b71071384718f03b0471774c
ltherisod <lauratherisod@gmail.com>
2025-10-13 20:05:09 -0300

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-// import BotonMultiuso from "./examples/BotonMultiuso"
-import Navbar from "./components/Navbar"
 import './App.css'
-import ItemListContainer from "./components/ItemListContainer"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ItemListContainer from "./components/ItemListContainer"
 import NavbarBS from "./components/NavbarBS";
 
 function App() {
@@ -10,19 +8,8 @@ function App() {
 
   return (
     <>
-    {/* PRIMER PRE ENTREGA */}
-      {/* <Navbar/> */}
       <NavbarBS/>
       <ItemListContainer saludo='Bienvenido a mi app'/>
-    {/* CODIGO DE EJEMPLO */}
-      {/* <BotonMultiuso label='Boton UNO' apagado={false} style='boton-uno'>
-        Boton Uno
-       {/* <p>Hola soy una children</p>
-        <p>Hola soy una children</p>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSl4y2IvI-kKYAL9aMAKBSQk8Yw2dQmGuZ_QA&s" alt='palito'/> */}
-      {/* </BotonMultiuso> */}
-      {/* idealmente asi, si no tiene hijo */}
-      {/* <BotonMultiuso label='BOTON DOS' apagado={true} style='boton-dos'>boton dos</BotonMultiuso> */} 
     </>
   )
 }
